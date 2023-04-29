33341b9a58121f5139e8cb147c190a261a26ca7a
Ieva Bareikė <82714279+ievapb@users.noreply.github.com>
2023-04-30 02:33:41 +0300

    Create No_code_error.py

---
+++ No_code_error.py
@@ -0,0 +1,10 @@
+# created 2023-04-29
+
+def bandom (x, y=1, z=2):
+    return x + y + z
+
+def smth (*args):  
+    raise NotImplementedError ("Missing code, baby!")
+
+print (bandom(y=3, x=2))
+print (smth(2))
